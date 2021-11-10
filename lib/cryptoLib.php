2206ac3c5c8d5fcb7070f3b460993af2d4033b79
becarioEducem <74835258+becarioEducem@users.noreply.github.com>
2021-11-10 22:20:27 +0100

    Optimitzant el codi...
    Ara si que no em faran fora! Llam d'optimització que estic preparant...

--- lib/cryptoLib.php
+++ lib/cryptoLib.php
@@ -10,3 +10,21 @@
         }
         return $coinsArray;
     }
+    
+    //TODO: CAL ACABAR LA FUNCIÓ
+    //TODO: @PARAM1 = ARRAY DE CRYPTODIVISES QUE CAL QUE PRESENTI LA GRID
+    //TODO: @PARAM2 = ARRAY DE CRYPTODIVISES AMB SEGUIMENT
+    function getHTMLCoinsGrid(){
+        $grid = "  <table class=\"table table-hover border border-primary rounded\"><thead><tr class=\"table-primary\">
+        <th>Crypto</th>
+        <th>Símbol</th>
+        <th>#Cap.Mercat</th>
+        <th>Icona</th>
+        <th>Preu BTC</th>
+        <th colspan=\"2\">Seguiment</th>
+        </thead></tr><tbody>";
+        //TODO: CALDRÀ RECUPERAR LES DADES DE CADA CRYPTO PER ANAR GENERANT LES FILES DE LA TAULA
+        //TODO: INVESTIGAR VIA DEPURACIÓ EL FORMAT DE LES DADES RETORNADES PER GETTRENDINGCOINS()
+        $grid  .= "</tbody></table>";
+        return $grid ;
+    }
