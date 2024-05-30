3d30a5cd7ce328d9d6c60320e9cc67251e0b51ff
becarioEducem <74835258+becarioEducem@users.noreply.github.com>
2024-05-30 17:33:38 +0200

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
     <link rel="icon" href="./img/favicon.png" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
     <link rel="stylesheet" href="./css/main.css" />
-    <title>Crypto Trends - Examen Parcial M7UF1 - DAM2</title>
+    <title>Crypto Trends - Examen Recuperació M7UF1 - DAM2</title>
 </head>
 <body>
     <h1 class="m-5">Crypto Trends</h1>
